6ee9c482295dd84f9772c9e895f940118d133238
Nathaniel James <nathanieltjames24@gmail.com>
2024-05-11 21:18:50 -0400

    scace services front end

--- app/src/services/spaces.tsx
+++ app/src/services/spaces.tsx
@@ -1,12 +1,17 @@
 import { axios } from "../config/axios";
 import _axios from "axios";
 
+interface SpaceProps {
+    logo: string,
+    name: string,
+}
+
 export const getUserSpaces = async (uId: string) => {
   let cancel: any;
 
   axios({
     method: "GET",
-    url: `spaces/${uId}`,
+    url: `spaces/user/${uId}`,
     cancelToken: new _axios.CancelToken((c) => (cancel = c)),
   })
     .then((res) => { })
@@ -16,5 +21,71 @@ export const getUserSpaces = async (uId: string) => {
 
   return () => cancel();
 };
+
+export const getSpace = async (spaceId: string) => {
+    let cancel: any;
+  
+    axios({
+      method: "GET",
+      url: `spaces/${spaceId}`,
+      cancelToken: new _axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => { })
+      .catch((e) => {
+        if (_axios.isCancel(e)) return;
+      });
+  
+    return () => cancel();
+  };
+
+  export const postSpace = async (space: SpaceProps) => {
+    let cancel: any;
+  
+    axios({
+      method: "POST",
+      url: `spaces/${spaceId}`,
+      data: space,
+      cancelToken: new _axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => { })
+      .catch((e) => {
+        if (_axios.isCancel(e)) return;
+      });
+  
+    return () => cancel();
+  };
+
+  export const putSpace = async (space: SpaceProps) => {
+    let cancel: any;
+  
+    axios({
+      method: "PUT",
+      url: `spaces/${spaceId}`,
+      data: space,
+      cancelToken: new _axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => { })
+      .catch((e) => {
+        if (_axios.isCancel(e)) return;
+      });
+  
+    return () => cancel();
+  };
+
+  export const deleteSpace = async (spaceId: string) => {
+    let cancel: any;
+  
+    axios({
+      method: "DELETE",
+      url: `spaces/${spaceId}`,
+      cancelToken: new _axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => { })
+      .catch((e) => {
+        if (_axios.isCancel(e)) return;
+      });
+  
+    return () => cancel();
+  };
 
 
