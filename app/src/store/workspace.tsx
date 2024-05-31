72de2bcba8c6b8a0671422782a75f1e2ee323444
Nathaniel James <nathanieltjames24@gmail.com>
2024-05-30 21:27:50 -0400

    start the workspace store

--- app/src/store/workspace.tsx
+++ app/src/store/workspace.tsx
@@ -1,13 +1,31 @@
 // src/store/useAuthStore.js
 import { create } from 'zustand';
-import { auth } from '../config/firebase';
-import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
-import { axios } from '../config/axios';
+import { Space } from '../interfaces/space';
 
-const useWorkspaceStore = create((set) => ({
+
+interface SpaceState {
+  loading: boolean;
+  active: Space | null;
+  spaces: Space[];
+  search: string;
+  getUserSpaces: (userId: string) => Promise<void>,
+  getSpaceById: (id: number) => Promise<void>,
+  createSpace: (name: string) => Promise<void>,
+  updateSpace: (space: Space) => Promise<void>,
+  archiveSpace: (id: number) => Promise<void>,
+}
+
+const useWorkspaceStore = create<SpaceState>((set) => ({
+  loading: false,
   active: null,
   spaces: [],
-  
+  search: "",
+  getUserSpaces: async (userId: string) => { },
+  getSpaceById: async (id: number) => { },
+  createSpace: async (name: string) => { },
+  updateSpace: async (space: Space) => { },
+  archiveSpace: async (id: number) => { },
+
 }));
 
 export default useWorkspaceStore;
